8d4198d2be2ae567657ef4e64f3f6c1daf5ddd64
kehman18 <kehindeadekola96@gmail.com>
2024-10-13 14:40:20 +0100

    edited the index.html file

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
-'''this are all the modules needed to get things done'''
-
+'''necessary for the server to run properly'''
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Route for the home page
 @app.route('/')
 def home_page():
-    '''this is the function to the home page'''
     return render_template('index.html')
+
+# Route for the contact page
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# Dynamic route for other HTML pages
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
